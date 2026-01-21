042937f16a822a6a665e78f62a19c94960a79e07
Clément Desberg <clement@desberg.fr>
2026-01-21 04:54:01 +0100

    fixed summaries

--- src/summaries.py
+++ src/summaries.py
@@ -2,6 +2,4 @@ from src.lib_sum_exp_results import load_experiments
 
 from json_extractor import get_all_data_for_streamlit
 
-# To complete
-
 # load_experiments returns the metrics and history of all measures
