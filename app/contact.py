b632f1bee1535a8c4a62d9565d363d491c0ee0f2
Clément Desberg <clement@desberg.fr>
2026-01-21 05:07:29 +0100

    updated contact page

--- app/contact.py
+++ app/contact.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.title("Contact")
 
-st.header("Clément Desberg")
+st.header("Developpers and Helpers")
+st.page_link("https://github.com/Cleonardino", label="Clément Desberg")
+st.page_link("https://github.com/nath54", label="Nathan Cerisara")
+st.page_link("https://github.com/Semeth0", label="Lucas Levy")
+st.page_link("https://github.com/Noza-e", label="Ysée Jacquet")
